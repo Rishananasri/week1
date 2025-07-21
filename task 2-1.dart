9290641aec17d2a94601ffdf03a354df833bc965
Rishananasri <rishananasri203@gmail.com>
2025-07-21 11:50:25 +0530

    Create task 2-1.dart

---
+++ task 2-1.dart
@@ -0,0 +1,26 @@
+void main(){
+  int day=7;
+  switch(day){
+    case 1:
+      print("its Sunday");
+      break;
+    case 2:
+      print("its Monday");
+      break;
+    case 3:
+      print("its Tuesday");
+      break;
+    case 4:
+      print("its Wednesday");
+      break;
+    case 5:
+      print("its Thursday");
+      break;
+    case 6:
+      print("its Friday");
+      break;
+    case 7:
+      print("its Saturday");
+      break;
+  }
+}
