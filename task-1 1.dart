d80c7bb6b9f070c262fcffe88744f5aaf811f4b9
Rishananasri <rishananasri203@gmail.com>
2025-07-21 11:36:42 +0530

    Create task-1 1.dart

---
+++ task-1 1.dart
@@ -0,0 +1,13 @@
+void main(){
+  int mark=24;
+  if(mark<=20){
+    print("you are fail");
+  }
+  else if(mark>20&&mark<70){
+    print("passed,but improve more");
+  }
+ else if(mark>=70&&mark<=100){
+   print("passed,keep it up");
+ }
+  else{print("invalid mark");}
+}
